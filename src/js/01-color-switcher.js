612d47501bf93cdd55ff0555d5cc201e06b6da25
AllaKaplia <118439299+AllaKaplia@users.noreply.github.com>
2023-04-16 20:22:29 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const TIME_COLOR_CHANGES = 1000;
+let timeChange = null;
 
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+
+function getRandomHexColor() {
+   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+startBtn.addEventListener('click', onStartClick);
+stopBtn.addEventListener('click', onStopClick);
+
+function onStartClick() {
+    timeChange = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+        startBtn.setAttribute('disabled', 'disabled');
+        stopBtn.removeAttribute('disabled');
+    }, TIME_COLOR_CHANGES);
+}
+
+function onStopClick() {
+    clearInterval(timeChange);
+    startBtn.removeAttribute('disabled');
+    stopBtn.setAttribute('disabled', 'disabled');
+}
