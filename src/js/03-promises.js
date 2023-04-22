674aa84d42add2e2888da8ee03f9c32652b802a1
AllaKaplia <118439299+AllaKaplia@users.noreply.github.com>
2023-04-22 23:38:17 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,53 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+const formEl = document.querySelector('.form');
+const buttonEl = document.querySelector('button');
+
+
+let amount = document.querySelector('input[name=amount]').value;
+let delay = document.querySelector('input[name=delay]').value;
+let step = document.querySelector('input[name=step]').value;
+
+
+let promptCounter = 0;
+
+formEl.addEventListener('submit', onValueRecords);
+buttonEl.addEventListener('click', onPromisesStart);
+
+function onValueRecords(evt) {
+  evt.preventDefault()
+  
+};
+
+function onPromisesStart() {
+  createPromise()
+};
+
+function createPromise(position, delay, amount) {
+  return new Promise ((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setInterval(() => {
+      if(promptCounter === amount) {
+        return;
+      }
+      promptCounter += 1;
+
+      if (shouldResolve) {
+        finally(`✅ Fulfilled promise ${position} in ${delay}ms`)
+      } else {
+        reject(`❌ Rejected promise ${position} in ${delay}ms`)
+      }
+    }, delay);
+  })
 }
+
+createPromise().then((position, delay) => onPromisesSuccess).catch((position, delay) => onPromisesError);
+
+function onPromisesSuccess() {
+  console.log(result);
+}
+
+function onPromisesError() {
+  console.log(error);
+}
+
